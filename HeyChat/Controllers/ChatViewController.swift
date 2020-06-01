b473bbca687f9fbe92fe13cb946ed1f57f514f3b
Aman Ilyasovich <aman.ilyasovich@gmail.com>
2020-05-31 20:49:18 -0700

    no message

---
+++ HeyChat/Controllers/ChatViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChatViewController.swift
+//  HeyChat
+//
+//  Created by Aman Ilyasovich on 5/25/20.
+//  Copyright © 2020 Aman Ilyasovich. All rights reserved.
+//
+
+import Foundation
---
+++ HeyChat/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  HeyChat
+//
+//  Created by Aman Ilyasovich on 5/25/20.
+//  Copyright © 2020 Aman Ilyasovich. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
