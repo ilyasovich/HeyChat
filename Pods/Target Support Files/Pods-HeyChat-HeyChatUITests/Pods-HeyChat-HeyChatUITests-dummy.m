4b2d1e8dfca79b75b9cf21798616a875912a286c
Aman Ilyasovich <aman.ilyasovich@gmail.com>
2020-06-13 18:25:28 -0700

    Pod update

---
+++ Pods/Target Support Files/Pods-HeyChat-HeyChatUITests/Pods-HeyChat-HeyChatUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HeyChat_HeyChatUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HeyChat_HeyChatUITests
+@end
---
+++ Pods/Target Support Files/Pods-HeyChatTests/Pods-HeyChatTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HeyChatTests : NSObject
+@end
+@implementation PodsDummy_Pods_HeyChatTests
+@end
